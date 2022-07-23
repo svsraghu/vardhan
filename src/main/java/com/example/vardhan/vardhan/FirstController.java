ff2d789ae96d4a2a2428b5a83ec200c8055c2bf7
svsraghu <svsrvardhan@gmail.com>
2022-07-23 18:39:02 +0530

    first commit

---
+++ src/main/java/com/example/vardhan/vardhan/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.vardhan.vardhan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+	
+	@GetMapping("/raghu")
+	public String hello() {
+		return "hello raghu";
+	}
+
+}
